1e30e6e95cd1288a0a9d41a2709ed6d662dbcf28
Taketsuru <taketsuru11@gmail.com>
2014-10-11 11:44:01 +0900

    Add severe() methods to Logger.

--- src/main/java/jp/dip/myuminecraft/takenomics/Logger.java
+++ src/main/java/jp/dip/myuminecraft/takenomics/Logger.java
@@ -8,24 +8,32 @@ public class Logger {
         this.logger = logger;
     }
     
-    public void warning(String format, Object...args) {
-        logger.warning(String.format(null, format, args));
-    }
-
     public void info(String format, Object...args) {
         logger.info(String.format(null, format, args));
     }
 
-    public void printStackTrace(Throwable th) {
-        for (StackTraceElement t : th.getStackTrace()) {
-            logger.warning(t.toString());
-        }
+    public void warning(String format, Object...args) {
+        logger.warning(String.format(null, format, args));
+    }
+
+    public void severe(String format, Object...args) {
+        logger.severe(String.format(null, format, args));
     }
 
     public void warning(Throwable th, String format, Object... args) {
         logger.warning(String.format(null, format, args));
         logger.warning(th.toString());
-        printStackTrace(th);
+        for (StackTraceElement t : th.getStackTrace()) {
+            logger.warning(t.toString());
+        }
+    }
+
+    public void severe(Throwable th, String format, Object... args) {
+        logger.severe(String.format(null, format, args));
+        logger.severe(th.toString());
+        for (StackTraceElement t : th.getStackTrace()) {
+            logger.severe(t.toString());
+        }
     }
 
 }
